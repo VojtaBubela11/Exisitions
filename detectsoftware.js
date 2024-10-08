54ab97dc62e31d6c0f9468ab4ca8aa9bc550c796
VojtaBubela11 <vojtabubela11@gmail.com>
2024-10-08 18:07:59 +0200

    Create detectsoftware.js

---
+++ detectsoftware.js
@@ -0,0 +1,13 @@
+  getInfo() {
+    return {
+      id: 'helloworld',
+      name: 'It works!',
+      blocks: [
+        {
+          opcode: 'hello',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Hello, world!'
+        }
+      ]
+    };
+  }
